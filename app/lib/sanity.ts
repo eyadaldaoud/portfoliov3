3bfa7552bd6e6b13b3e6ccb7ed7d6c7c10b00cf1
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-06-16 20:22:50 -0400

    fixed client issue

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: "sl4fx8oc",
+    dataset: "production",
     useCdn: true,
     apiVersion: '2023-05-03',
   })
