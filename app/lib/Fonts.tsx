ead503397ff12a33bd8333dc11fe96dcf5fa02e3
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-10-14 16:19:35 -0400

    Free_palestine

---
+++ app/lib/Fonts.tsx
@@ -0,0 +1,3 @@
+import { Amiri_Quran } from "next/font/google";
+
+export const arabic = Amiri_Quran({ weight: "400", subsets: ["arabic"] });
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Accounts from "./Components/Accounts";
 import Projects from "./Components/Projects";
+import { arabic } from "./lib/Fonts";
 
 export default async function Home() {
   return (
     <>
+      <h1 className={`${arabic.className} mt-8 mb-8 text-center text-5xl`}>
+        فَأَخَذَهُمُ الطُّوفَانُ وَهُمْ ظَالِمُونَ
+      </h1>
+      <h1 className="text-center text-xl font-serif">
+        and the flood seized them while they were wrongdoers.
+      </h1>
+      <p className={`${arabic.className} text-center`}>طوفان_الاقصى#</p>
+      <p className="text-center text-sm  mt-2">#Toofan Al-Aqsa</p>
       <div className="flex justify-center mt-12">
         <Accounts />
       </div>
