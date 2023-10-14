16c48f50bb752d889789098feedfb8b035d5bb7a
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-10-14 16:26:10 -0400

    free_palestine

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,18 @@ import { arabic } from "./lib/Fonts";
 export default async function Home() {
   return (
     <>
-      <h1 className={`${arabic.className} mt-8 mb-8 text-center text-5xl`}>
+      <h1
+        className={`${arabic.className} mt-8 mb-8 text-center sm:text-5xl text-4xl`}
+      >
         فَأَخَذَهُمُ الطُّوفَانُ وَهُمْ ظَالِمُونَ
       </h1>
-      <h1 className="text-center text-xl font-serif">
+      <h1 className="text-center sm:text-xl text-sm font-serif font-bold text-gray-300">
         and the flood seized them while they were wrongdoers.
       </h1>
-      <p className={`${arabic.className} text-center`}>طوفان_الاقصى#</p>
-      <p className="text-center text-sm  mt-2">#Toofan Al-Aqsa</p>
+      <p className={`${arabic.className} text-center text-gray-400`}>
+        طوفان_الاقصى#
+      </p>
+      <p className="text-center text-sm mt-2 text-gray-400">#Toofan Al-Aqsa</p>
       <div className="flex justify-center mt-12">
         <Accounts />
       </div>
