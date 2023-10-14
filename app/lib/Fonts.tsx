1837af03a3c7ea157ab8dcfb57320a442beb9c1c
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-10-14 16:53:43 -0400

    free_palestine

--- app/lib/Fonts.tsx
+++ app/lib/Fonts.tsx
@@ -1,3 +1,5 @@
-import { Reem_Kufi_Fun } from "next/font/google";
+import { Kanit, Reem_Kufi_Fun } from "next/font/google";
 
 export const arabic = Reem_Kufi_Fun({ weight: "400", subsets: ["arabic"] });
+
+export const en = Kanit({ weight: "500", subsets: ["latin"] });
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 import Accounts from "./Components/Accounts";
 import Projects from "./Components/Projects";
-import { arabic } from "./lib/Fonts";
+import { arabic, en } from "./lib/Fonts";
 
 export default async function Home() {
   return (
     <>
-      <h1 className={` mt-8 mb-8 text-center sm:text-5xl text-4xl`}>
-        فَأَخَذَهُمُ الطُّوفَانُ وَهُمْ ظَالِمُونَ
-      </h1>
-      <h1 className="text-center sm:text-xl text-sm font-serif font-bold text-gray-300">
-        and the flood seized them while they were wrongdoers.
-      </h1>
-      <p className={`${arabic.className} text-center text-gray-400`}>
-        طوفان_الاقصى#
-      </p>
-      <p className="text-center text-sm mt-2 text-gray-400">#Toofan Al-Aqsa</p>
+      <div className="text-center">
+        <h1 className={` mt-8 mb-4  sm:text-5xl text-4xl`}>
+          فَأَخَذَهُمُ الطُّوفَانُ وَهُمْ ظَالِمُونَ
+        </h1>
+        <h1 className="sm:text-xl text-sm font-serif font-bold dark:text-gray-300 text-gray-700 mb-4">
+          and the flood seized them while they were wrongdoers.
+        </h1>
+        <h2 className={`${en.className}`}>
+          <q>IT ISN’T SELF-DEFENSE IF YOU ARE AN OCCUPYING FORCE</q>
+        </h2>
+        <p className={`${arabic.className} text-gray-400 mt-2`}>
+          طوفان_الاقصى#
+        </p>
+        <p className={`${en.className} text-sm mt-2 text-gray-400`}>
+          #Toofan_Al-Aqsa
+        </p>
+      </div>
+
       <div className="flex justify-center mt-12">
         <Accounts />
       </div>
