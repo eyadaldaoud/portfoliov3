7904308472f693af485d6ff3b584e54751148734
Eyad Al-Daoud <103299832+eyadaldaoud@users.noreply.github.com>
2025-03-24 00:16:46 +0300

    Update Footer.tsx

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024 <strong className="hover:underline">Eyad's Portfolio</strong>.
+          <strong className="hover:underline">Eyad's Portfolio</strong>.
           All Rights Reserved.
         </span>
       </div>
